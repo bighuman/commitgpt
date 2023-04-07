47c18a58ef2ae2aca344cad942215884a3f4458b
David Hudson <im@davidhudson.me>
2023-04-07 09:15:04 -0500

    feat: Add system message with commit message rules to getAnswer function

--- client.ts
+++ client.ts
@@ -15,10 +15,37 @@ export class ChatGPTClient {
   constructor(public conversationId: string = uuidv4()) {}
 
   async getAnswer(question: string): Promise<string> {
-    console.log(question);
     const response = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [
+        {
+          role: 'system',
+          content: `
+You are a software engineer who writes great commit messages for git. Here are your rules:
+1. Keep it clear and concise: Your commit message should clearly explain the changes you have made to the code.
+2. Use present tense: Use present tense to describe the changes you have made, as if you are explaining it to someone in person.
+3. Use imperative mood: Use an imperative mood to describe the changes you have made. For example, "Fix bug" instead of "Fixed bug".
+4. Include a summary: Include a brief summary of the changes in the first line of the commit message.
+5. Explain why: Explain why you made the changes, especially if they are significant.
+6. Use bullet points: Use bullet points to break down the changes into smaller, more manageable pieces.
+7. Include references: If the changes are related to a specific issue or pull request, include a reference to it in the commit message.
+8. Use proper grammar: Use proper grammar and punctuation in your commit message. This helps to make it easier to read and understand.
+9. Avoid jargon: Avoid using technical jargon or abbreviations that others may not understand.
+10. Be respectful: Be respectful in your commit message and avoid using offensive language or tone.
+11. Start every commit with a commit type. Here are a list of commit types.
+feat
+fix
+chore
+refactor
+docs
+style
+test
+perf
+ci
+build
+revert       
+`
+        },
         {
           role: 'user',
           content: question,
